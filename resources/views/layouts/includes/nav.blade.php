60ebb6908bf1c037d743b76f1d7726e2c06e0967
Ali Shaukat <alishaukat.giki@gmail.com>
2016-07-26 03:22:45 +0500

    Navigation updated

--- resources/views/layouts/includes/nav.blade.php
+++ resources/views/layouts/includes/nav.blade.php
@@ -9,17 +9,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Seth Movies</a>
+            <a class="navbar-brand" href="{{route('home')}}">Seth Movies</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li>
+<!--                <li>
                     <a href="#">About</a>
                 </li>
                 <li>
                     <a href="#">Contact</a>
-                </li>
+                </li>-->
             </ul>
         </div>
         <!-- /.navbar-collapse -->
